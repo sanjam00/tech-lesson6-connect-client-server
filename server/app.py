819ff77d03a7f05f49544dedc734ce07944dfd88
Sanaya James <sanaya.j.james@gmail.com>
2026-03-13 20:54:38 -0700

    completed lesson. connected frontend and backend with get and post

--- server/app.py
+++ server/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify, send_from_directory
+from store import get_sample_data
 # Import any helper functions you create in store.py
 
 app = Flask(__name__, static_folder='../client', static_url_path='')
 
 # TASK: Define a route for "/"
 # This should return index.html from the static folder
-
+@app.route("/", methods=["GET"])
+def index():
+	return send_from_directory(app.static_folder, "index.html")
 
 # TASK: Define a route for "/api/data"
 # This should return a JSON object with a message and status
 # Hint: You can use a function from store.py to generate the response
-
+@app.route("/api/data", methods=["GET"])
+def get_data():
+	return jsonify(get_sample_data()), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- server/store.py
+++ server/store.py
@@ -1,6 +1,7 @@
 # TASK: Create a function called get_sample_data
 # This function should return a dictionary with keys "message" and "status"
-# For example: { "message": "Hello from Flask!", "status": "success" }
 
-# def get_sample_data():
-#     pass
+data = { "message": "Hello from Flask!", "status": "success" }
+
+def get_sample_data():
+  return data
